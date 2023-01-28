dca3d1b994dfbe3e86b4fc6261520ccc78308dfc
zzungdo <jjungdo97@naver.com>
2023-01-28 16:34:54 +0900

    2nd style

---
+++ main.js
@@ -0,0 +1,66 @@
+const mbti = {
+    "INTJ":{
+        name:"INTJ",
+        label:"용의주도한 전략가"
+    },
+    "INTP":{
+        name:"INTP",
+        label:"논리적인 사색가"
+    },
+    "ENTJ":{
+        name:"ENTJ",
+        label:"대담한 통솔자"
+    },
+    "ENTP":{
+        name:"ENTP",
+        label:"뜨거운 논쟁을 즐기는 변론가"
+    },
+    "INFJ":{
+        name:"INFJ",
+        label:"선의의 옹호자"
+    },
+    "INFP":{
+        name:"INFP",
+        label:"열정적인 중재자"
+    },
+    "ENFJ":{
+        name:"ENFJ",
+        label:"정의로운 사회운동가"
+    },
+    "ENFP":{
+        name:"ENFP",
+        label:"재기발랄한 활동가"
+    },
+    "ISTJ":{
+        name:"ISTJ",
+        label:"청렴결백한 논리주의자"
+    },
+    "ISFJ":{
+        name:"ISFJ",
+        label:"용감한 수호자"
+    },
+    "ESTJ":{
+        name:"ESTJ",
+        label:"엄격한 관리자"
+    },
+    "ESFJ":{
+        name:"ESFJ",
+        label:"사교적인 외교관"
+    },
+    "ISTP":{
+        name:"ISTP",
+        label:"만능 재주꾼"
+    },
+    "ISFP":{
+        name:"ISFP",
+        label:"호기심 많은 예술가"
+    },
+    "ESTP":{
+        name:"ESTP",
+        label:"모험을 즐기는 사업가"
+    },
+    "ESFP":{
+        name:"ESFP",
+        label:"자유로운 영혼의 연예인"
+    }
+}
